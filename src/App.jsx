0c4e0418800808834220b9bd515409772dc1d405
Liam Cottrell <lemcottrell@gmail.com>
2024-02-16 19:43:49 +0000

    start gallery

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import Navbar from "./components/Navbar";
 import Home from "./Home";
+import Gallery from "./Gallery";
 
 function App() {
   return (
     <>
       <Navbar />
       <Home />
-      {/* <ProjectGallery />
-      <Contact />  */}
+      <Gallery />
+      {/* <Contact />  */}
     </>
   )
 }
---
+++ src/Gallery.jsx
@@ -0,0 +1,5 @@
+function Gallery() {
+    return <p>Cards will go here</p>
+}
+
+export default Gallery;
